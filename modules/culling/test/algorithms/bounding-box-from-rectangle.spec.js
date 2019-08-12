384180553c3036c8f7da2bc985231ff362d397f7
Ib Green <ib@uber.com>
2019-08-12 12:36:49 -0700

    wip

--- modules/culling/test/algorithms/bounding-box-from-rectangle.spec.js
+++ modules/culling/test/algorithms/bounding-box-from-rectangle.spec.js
@@ -1,4 +1,8 @@
-it('fromRectangle sets correct default ellipsoid', function() {
+import {it, expect} from 'test/utils/expect-assertions';
+
+import { OrientedBoundingBox } from '@math.gl/culling';
+
+it('fromRectangle sets correct default ellipsoid', function () {
   var rectangle = new Rectangle(-0.9, -1.2, 0.5, 0.7);
   var box1 = OrientedBoundingBox.fromRectangle(rectangle, 0.0, 0.0);
   var box2 = OrientedBoundingBox.fromRectangle(rectangle, 0.0, 0.0, Ellipsoid.WGS84);
@@ -8,7 +12,7 @@ it('fromRectangle sets correct default ellipsoid', function() {
   expect(box1.halfAxes).toEqualEpsilon(box2.halfAxes, CesiumMath.EPSILON15);
 });
 
-it('fromRectangle sets correct default heights', function() {
+it('fromRectangle sets correct default heights', () => {
   var rectangle = new Rectangle(0.0, 0.0, 0.0, 0.0);
   var box = OrientedBoundingBox.fromRectangle(rectangle, undefined, undefined, Ellipsoid.UNIT_SPHERE);
 
@@ -18,14 +22,14 @@ it('fromRectangle sets correct default heights', function() {
   expect(box.halfAxes).toEqualEpsilon(rotScale, CesiumMath.EPSILON15);
 });
 
-it('fromRectangle throws without rectangle', function() {
+it('fromRectangle throws without rectangle', () => {
   var ellipsoid = Ellipsoid.UNIT_SPHERE;
   expect(function() {
       OrientedBoundingBox.fromRectangle(undefined, 0.0, 0.0, ellipsoid);
   }).toThrowDeveloperError();
 });
 
-it('fromRectangle throws with invalid rectangles', function() {
+it('fromRectangle throws with invalid rectangles', () => {
   var ellipsoid = Ellipsoid.UNIT_SPHERE;
   expect(function() { return OrientedBoundingBox.fromRectangle(new Rectangle(-1.0, 1.0, 1.0, -1.0), 0.0, 0.0, ellipsoid); }).toThrowDeveloperError();
   expect(function() { return OrientedBoundingBox.fromRectangle(new Rectangle(-2.0, 2.0, -1.0, 1.0), 0.0, 0.0, ellipsoid); }).toThrowDeveloperError();
@@ -35,13 +39,13 @@ it('fromRectangle throws with invalid rectangles', function() {
   expect(function() { return OrientedBoundingBox.fromRectangle(new Rectangle(-4.0, -1.0, 4.0, 2.0), 0.0, 0.0, ellipsoid); }).toThrowDeveloperError();
 });
 
-it('fromRectangle throws with non-revolution ellipsoids', function() {
+it('fromRectangle throws with non-revolution ellipsoids', () => {
   var rectangle = new Rectangle(0.0, 0.0, 0.0, 0.0);
   expect(function() { return OrientedBoundingBox.fromRectangle(rectangle, 0.0, 0.0, new Ellipsoid(1.01, 1.00, 1.01)); }).toThrowDeveloperError();
   expect(function() { return OrientedBoundingBox.fromRectangle(rectangle, 0.0, 0.0, new Ellipsoid(1.00, 1.01, 1.01)); }).toThrowDeveloperError();
 });
 
-it('fromRectangle creates an OrientedBoundingBox without a result parameter', function() {
+it('fromRectangle creates an OrientedBoundingBox without a result parameter', () => {
   var ellipsoid = Ellipsoid.UNIT_SPHERE;
   var rectangle = new Rectangle(0.0, 0.0, 0.0, 0.0);
   var box = OrientedBoundingBox.fromRectangle(rectangle, 0.0, 0.0, ellipsoid);
@@ -52,7 +56,7 @@ it('fromRectangle creates an OrientedBoundingBox without a result parameter', fu
   expect(box.halfAxes).toEqualEpsilon(rotScale, CesiumMath.EPSILON15);
 });
 
-it('fromRectangle creates an OrientedBoundingBox with a result parameter', function() {
+it('fromRectangle creates an OrientedBoundingBox with a result parameter', () => {
   var ellipsoid = Ellipsoid.UNIT_SPHERE;
   var rectangle = new Rectangle(0.0, 0.0, 0.0, 0.0);
   var result = new OrientedBoundingBox();
@@ -65,7 +69,7 @@ it('fromRectangle creates an OrientedBoundingBox with a result parameter', funct
   expect(box.halfAxes).toEqualEpsilon(rotScale, CesiumMath.EPSILON15);
 });
 
-it('fromRectangle for rectangles with heights', function() {
+it('fromRectangle for rectangles with heights', () => {
   var d90 = CesiumMath.PI_OVER_TWO;
 
   var box;
@@ -95,7 +99,7 @@ it('fromRectangle for rectangles with heights', function() {
   expect(box.halfAxes).toEqualEpsilon(new Matrix3(0, 0, 0.5, 1, 0, 0, 0, 1, 0), CesiumMath.EPSILON15);
 });
 
-it('fromRectangle for interesting, degenerate, and edge-case rectangles', function() {
+it('fromRectangle for interesting, degenerate, and edge-case rectangles', () => {
   var d45 = CesiumMath.PI_OVER_FOUR;
   var d30 = CesiumMath.PI_OVER_SIX;
   var d90 = CesiumMath.PI_OVER_TWO;
@@ -165,40 +169,3 @@ it('fromRectangle for interesting, degenerate, and edge-case rectangles', functi
   expect(box.center).toEqualEpsilon(new Cartesian3(0.5, 0.0, 0.0), CesiumMath.EPSILON15);
   expect(box.halfAxes).toEqualEpsilon(new Matrix3(0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0), CesiumMath.EPSILON15);
 });
-
-var intersectPlaneTestCornersEdgesFaces = function(center, axes) {
-  var SQRT1_2 = Math.pow(1.0 / 2.0, 1 / 2.0);
-  var SQRT3_4 = Math.pow(3.0 / 4.0, 1 / 2.0);
-
-  var box = new OrientedBoundingBox(center, Matrix3.multiplyByScalar(axes, 0.5, new Matrix3()));
-
-  var planeNormXform = function(nx, ny, nz, dist) {
-      var n = new Cartesian3(nx, ny, nz);
-      var arb = new Cartesian3(357, 924, 258);
-      var p0 = Cartesian3.normalize(n, new Cartesian3());
-      Cartesian3.multiplyByScalar(p0, -dist, p0);
-      var tang = Cartesian3.cross(n, arb, new Cartesian3());
-      Cartesian3.normalize(tang, tang);
-      var binorm = Cartesian3.cross(n, tang, new Cartesian3());
-      Cartesian3.normalize(binorm, binorm);
-
-      Matrix3.multiplyByVector(axes, p0, p0);
-      Matrix3.multiplyByVector(axes, tang, tang);
-      Matrix3.multiplyByVector(axes, binorm, binorm);
-      Cartesian3.cross(tang, binorm, n);
-      if (Cartesian3.magnitude(n) === 0) {
-          return undefined;
-      }
-      Cartesian3.normalize(n, n);
-
-      Cartesian3.add(p0, center, p0);
-      var d = -Cartesian3.dot(p0, n);
-      if (Math.abs(d) > 0.0001 && Cartesian3.magnitudeSquared(n) > 0.0001) {
-          return new Plane(n, d);
-      }
-      return undefined;
-  };
-
-  var pl;
-
-  // Tests against faces
---
+++ modules/geospatial/test/ellipsoid/ellipsoid-tangent-plane.spec.js
@@ -0,0 +1,281 @@
+import {it, expect} from 'test/utils/expect-assertions';
+
+import { EllipsoidTangentPlane, Ellipsoid } from '@math.gl/geospatial';
+import { Vector2, Vector3 } from 'math.gl';
+
+const ELLIPSOID_UNIT_SPHERE = new Ellipsoid(1.0, 1.0, 1.0);
+const VECTOR3_UNIT_X = new Vector3(1, 0, 0);
+
+it('constructor defaults to WGS84', () => {
+  const origin = new Vector3(Ellipsoid.WGS84.radii.x, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin);
+  expect(tangentPlane.ellipsoid).toBe(Ellipsoid.WGS84);
+  expect(tangentPlane.origin).toEqual(origin);
+});
+
+it('constructor sets expected values', () => {
+  const tangentPlane = new EllipsoidTangentPlane(VECTOR3_UNIT_X, ELLIPSOID_UNIT_SPHERE);
+  expect(tangentPlane.ellipsoid).toBe(ELLIPSOID_UNIT_SPHERE);
+  expect(tangentPlane.origin).toEqual(VECTOR3_UNIT_X);
+});
+
+it('fromPoints sets expected values', () => {
+  const points = [new Vector3(2.0, 0.0, 0.0), new Vector3(0.0, 0.0, 0.0)];
+  const tangentPlane = EllipsoidTangentPlane.fromPoints(points, ELLIPSOID_UNIT_SPHERE);
+  expect(tangentPlane.ellipsoid).toBe(ELLIPSOID_UNIT_SPHERE);
+  expect(tangentPlane.origin).toEqual(VECTOR3_UNIT_X);
+});
+
+it('projectPointOntoPlane returns undefined for unsolvable projections', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+  const positions = new Vector3(0.0, 0.0, 1.0);
+  const returnedResult = tangentPlane.projectPointOntoPlane(positions);
+  expect(returnedResult).toBeUndefined();
+});
+
+it('projectPointOntoPlane works without a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = new Vector3(1.0, 0.0, 1.0);
+  const expectedResult = new Vector2(0.0, 1.0);
+  const returnedResult = tangentPlane.projectPointOntoPlane(positions);
+  expect(returnedResult).toEqual(expectedResult);
+});
+
+it('projectPointOntoPlane works with a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = new Vector3(1.0, 0.0, 1.0);
+  const expectedResult = new Vector2(0.0, 1.0);
+  const result = new Vector2();
+  const returnedResult = tangentPlane.projectPointOntoPlane(positions, result);
+  expect(result).toBe(returnedResult);
+  expect(returnedResult).toEqual(expectedResult);
+});
+
+it('projectPointsOntoPlane works without a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(1.0, 0.0, 1.0), new Vector3(1.0, 0.0, 0.0), new Vector3(1.0, 1.0, 0.0)];
+  const expectedResults = [new Vector2(0.0, 1.0), new Vector2(0.0, 0.0), new Vector2(1.0, 0.0)];
+  const returnedResults = tangentPlane.projectPointsOntoPlane(positions);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('projectPointsOntoPlane works with a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(1.0, 0.0, 1.0), new Vector3(1.0, 0.0, 0.0), new Vector3(1.0, 1.0, 0.0)];
+  const expectedResults = [new Vector2(0.0, 1.0), new Vector2(0.0, 0.0), new Vector2(1.0, 0.0)];
+
+  const index0 = new Vector2();
+  const result = [index0];
+  const returnedResults = tangentPlane.projectPointsOntoPlane(positions, result);
+  expect(result).toBe(returnedResults);
+  expect(result[0]).toBe(index0);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('projectPointsOntoPlane works when some points cannot be projected', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(1.0, 0.0, 1.0), new Vector3(1.0, 0.0, 0.0), new Vector3(0.0, 0.0, 1.0), new Vector3(1.0, 1.0, 0.0), new Vector3(0.0, 1.0, 0.0)];
+  const expectedResults = [new Vector2(0.0, 1.0), new Vector2(0.0, 0.0), new Vector2(1.0, 0.0)];
+  const returnedResults = tangentPlane.projectPointsOntoPlane(positions);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('projectPointOntoEllipsoid works without a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const position = new Vector3(2.0, 2.0, 0.0);
+  const expectedResult = new Vector3(1.0/3.0, 2.0/3.0,  2.0/3.0);
+  const returnedResult = tangentPlane.projectPointOntoEllipsoid(position);
+  expect(returnedResult).toEqual(expectedResult);
+});
+
+it('projectPointOntoEllipsoid works with a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const position = new Vector3(2.0, -2.0, 0.0);
+  const expectedResult = new Vector3(1.0/3.0, 2.0/3.0, -2.0/3.0);
+  const result = new Vector3();
+  const returnedResult = tangentPlane.projectPointOntoEllipsoid(position, result);
+  expect(result).toBe(returnedResult);
+  expect(returnedResult).toEqual(expectedResult);
+});
+
+it('projectPointsOntoEllipsoid works without a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(2.0, -2.0, 0.0), new Vector3(2.0, 2.0, 0.0)];
+  const expectedResults = [new Vector3(1.0/3.0, 2.0/3.0, -2.0/3.0), new Vector3(1.0/3.0, 2.0/3.0,  2.0/3.0)];
+  const returnedResults = tangentPlane.projectPointsOntoEllipsoid(positions);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('projectPointsOntoEllipsoid works with a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(2.0, -2.0, 0.0), new Vector3(2.0, 2.0, 0.0)];
+  const expectedResults = [new Vector3(1.0/3.0, 2.0/3.0, -2.0/3.0), new Vector3(1.0/3.0, 2.0/3.0,  2.0/3.0)];
+  const index0 = new Vector3();
+  const result = [index0];
+  const returnedResults = tangentPlane.projectPointsOntoEllipsoid(positions, result);
+  expect(result).toBe(returnedResults);
+  expect(result[0]).toBe(index0);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('projectPointToNearestOnPlane works without a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = new Vector3(1.0, 0.0, 1.0);
+  const expectedResult = new Vector2(0.0, 1.0);
+  const returnedResult = tangentPlane.projectPointToNearestOnPlane(positions);
+  expect(returnedResult).toEqual(expectedResult);
+});
+
+it('projectPointToNearestOnPlane works projecting from constious distances', function () {
+  const ellipsoid = Ellipsoid.ZERO;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  expect(tangentPlane.projectPointToNearestOnPlane(new Vector3(2.0, 0.0, 0.0))).toEqual(new Vector2(0.0, 0.0));
+  expect(tangentPlane.projectPointToNearestOnPlane(new Vector3(1.0, 0.0, 0.0))).toEqual(new Vector2(0.0, 0.0));
+  expect(tangentPlane.projectPointToNearestOnPlane(new Vector3(0.0, 0.0, 0.0))).toEqual(new Vector2(0.0, 0.0));
+  expect(tangentPlane.projectPointToNearestOnPlane(new Vector3(-1.0, 0.0, 0.0))).toEqual(new Vector2(0.0, 0.0));
+});
+
+it('projectPointToNearestOnPlane works with a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = new Vector3(1.0, 0.0, 1.0);
+  const expectedResult = new Vector2(0.0, 1.0);
+  const result = new Vector2();
+  const returnedResult = tangentPlane.projectPointToNearestOnPlane(positions, result);
+  expect(result).toBe(returnedResult);
+  expect(returnedResult).toEqual(expectedResult);
+});
+
+it('projectPointsToNearestOnPlane works without a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(1.0, 0.0, 1.0), new Vector3(1.0, 0.0, 0.0), new Vector3(1.0, 1.0, 0.0)];
+  const expectedResults = [new Vector2(0.0, 1.0), new Vector2(0.0, 0.0), new Vector2(1.0, 0.0)];
+  const returnedResults = tangentPlane.projectPointsToNearestOnPlane(positions);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('projectPointsToNearestOnPlane works with a result parameter', function () {
+  const ellipsoid = ELLIPSOID_UNIT_SPHERE;
+  const origin = new Vector3(1.0, 0.0, 0.0);
+  const tangentPlane = new EllipsoidTangentPlane(origin, ellipsoid);
+
+  const positions = [new Vector3(1.0, 0.0, 1.0), new Vector3(1.0, 0.0, 0.0), new Vector3(1.0, 1.0, 0.0)];
+  const expectedResults = [new Vector2(0.0, 1.0), new Vector2(0.0, 0.0), new Vector2(1.0, 0.0)];
+
+  const index0 = new Vector2();
+  const result = [index0];
+  const returnedResults = tangentPlane.projectPointsToNearestOnPlane(positions, result);
+  expect(result).toBe(returnedResults);
+  expect(result[0]).toBe(index0);
+  expect(returnedResults).toEqual(expectedResults);
+});
+
+it('constructor throws without origin', () => {
+  expect(() => {
+      return new EllipsoidTangentPlane(undefined, Ellipsoid.WGS84);
+  }).toThrowDeveloperError();
+});
+
+it('constructor throws if origin is at the center of the ellipsoid', () => {
+  expect(() => {
+      return new EllipsoidTangentPlane(Vector3.ZERO, Ellipsoid.WGS84);
+  }).toThrowDeveloperError();
+});
+
+it('fromPoints throws without cartesians', () => {
+  expect(() => {
+      return EllipsoidTangentPlane.fromPoints(undefined, Ellipsoid.WGS84);
+  }).toThrowDeveloperError();
+});
+
+it('projectPointOntoPlane throws without cartesian', () => {
+  const tangentPlane = new EllipsoidTangentPlane(VECTOR3_UNIT_X, ELLIPSOID_UNIT_SPHERE);
+  expect(() => {
+      return tangentPlane.projectPointOntoPlane(undefined);
+  }).toThrowDeveloperError();
+});
+
+it('projectPointsOntoPlane throws without cartesians', () => {
+  const tangentPlane = new EllipsoidTangentPlane(VECTOR3_UNIT_X, ELLIPSOID_UNIT_SPHERE);
+  expect(() => {
+      return tangentPlane.projectPointsOntoPlane(undefined);
+  }).toThrowDeveloperError();
+});
+
+it('projectPointToNearestOnPlane throws without cartesian', () => {
+  const tangentPlane = new EllipsoidTangentPlane(VECTOR3_UNIT_X, ELLIPSOID_UNIT_SPHERE);
+  expect(() => {
+      return tangentPlane.projectPointToNearestOnPlane(undefined);
+  }).toThrowDeveloperError();
+});
+
+it('projectPointsToNearestOnPlane throws without cartesians', () => {
+  const tangentPlane = new EllipsoidTangentPlane(VECTOR3_UNIT_X, ELLIPSOID_UNIT_SPHERE);
+  expect(() => {
+      return tangentPlane.projectPointsToNearestOnPlane(undefined);
+  }).toThrowDeveloperError();
+});
+
+it('projectPointsOntoEllipsoid throws without cartesians', () => {
+  const tangentPlane = new EllipsoidTangentPlane(VECTOR3_UNIT_X, ELLIPSOID_UNIT_SPHERE);
+  expect(() => {
+      return tangentPlane.projectPointsOntoEllipsoid(undefined);
+  }).toThrowDeveloperError();
+});
+
+it('projectPointsOntoEllipsoid works with an arbitrary ellipsoid using fromPoints', function () {
+  const points = Vector3.fromDegreesArray([
+      -72.0, 40.0,
+      -68.0, 35.0,
+      -75.0, 30.0,
+      -70.0, 30.0,
+      -68.0, 40.0
+  ]);
+
+  const tangentPlane = EllipsoidTangentPlane.fromPoints(points, Ellipsoid.WGS84);
+  const points2D = tangentPlane.projectPointsOntoPlane(points);
+  const positionsBack = tangentPlane.projectPointsOntoEllipsoid(points2D);
+
+  expect(positionsBack[0].x).toBeCloseTo(points[0].x);
+  expect(positionsBack[0].y).toBeCloseTo(points[0].y);
+  expect(positionsBack[0].z).toBeCloseTo(points[0].z);
+});
